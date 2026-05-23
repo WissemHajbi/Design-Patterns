710cb72196ccc62f0a38f07ca302dbe0683f4e1d
Wissem Hajbi <wissemhajbi2002@gmail.com>
2026-05-23 11:10:59 +0100

    :recycle: add javafx module

--- src/factory/ShapeCreator.java
+++ src/factory/ShapeCreator.java
@@ -2,7 +2,6 @@ package factory;
 
 import model.DrawableShape;
 
-// Factory interface for creating one kind of shape.
 public interface ShapeCreator {
     String getType();
     DrawableShape create(ShapeRequest request);
